2d4d23af363cc00ad614fd3bc2b37219385ce0dd
Christoph Omid Hohnerlein <christoph.o.hohnerlein@campus.tu-berlin.de>
2015-04-03 16:48:33 +0200

    Capitalize DB columns

--- models/item.js
+++ models/item.js
@@ -4,21 +4,21 @@ module.exports = function(sequelize, DataTypes) {
   var Item = sequelize.define("Item", {
 
       // Label with custom getter that zero fills 4 chars
-      label: {
+      Label: {
         type: DataTypes.INTEGER(4).UNSIGNED,
         allowNull: false,
         get: function(){
-          return ("000"+this.getDataValue('label')).slice(-4);
+          return ("000"+this.getDataValue('Label')).slice(-4);
         }
       },
-      name: {type: DataTypes.TEXT, allowNull: false,notNull: true},
-      description: DataTypes.TEXT,
-      category: DataTypes.TEXT,
-      url: DataTypes.STRING,
-      location: DataTypes.TEXT, // room shelf.level
-      status: DataTypes.STRING, //in,out,nocirc
-      condition: DataTypes.STRING, // good, mended, broken
-      comment: DataTypes.TEXT //repair history etc
+      Item: {type: DataTypes.TEXT, allowNull: false,notNull: true},
+      Description: DataTypes.TEXT,
+      Category: DataTypes.TEXT,
+      URL: DataTypes.STRING,
+      Location: DataTypes.TEXT, // room shelf.level
+      Status: DataTypes.STRING, //in,out,nocirc
+      Condition: DataTypes.STRING, // good, mended, broken
+      Comment: DataTypes.TEXT //repair history etc
     }, {
     classMethods: {
       associate: function(models) {
