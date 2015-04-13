c09e21f5de9ff03762524d1c20bac28b74eb0bae
Christoph Omid Hohnerlein <christoph.o.hohnerlein@campus.tu-berlin.de>
2015-04-13 03:14:49 +0200

    temp comment unfinished userTable

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -153,27 +153,27 @@ window.onload = function() {
 
 
   // Initialize DataTable for user
-  var userTableList = $('#borrowedTable').DataTable({
-    // Data source: ajax call to /listItems, where 'items' object is passed
-    ajax:  {
-      url: '/checkUserItems',
-      dataSrc: 'items'
-    },
-    paging: false,  // turn of paging
-    // Extract each column value from a different object variable
-    columns: [
-      { data: 'Category' },
-      { data: 'Item' },
-      { data: 'Label' },
-      { data: 'Location' },
-      { data: 'Condition' }
-    ],
-    order: [[2, 'asc']], // Order by label
-    columnDefs: [{
-      targets: '_all',
-      defaultContent: ''
-    }] 
-  });
+  // var userTableList = $('#borrowedTable').DataTable({
+  //   // Data source: ajax call to /listItems, where 'items' object is passed
+  //   ajax:  {
+  //     url: '/checkUserItems',
+  //     dataSrc: 'items'
+  //   },
+  //   paging: false,  // turn of paging
+  //   // Extract each column value from a different object variable
+  //   columns: [
+  //     { data: 'Category' },
+  //     { data: 'Item' },
+  //     { data: 'Label' },
+  //     { data: 'Location' },
+  //     { data: 'Condition' }
+  //   ],
+  //   order: [[2, 'asc']], // Order by label
+  //   columnDefs: [{
+  //     targets: '_all',
+  //     defaultContent: ''
+  //   }] 
+  // });
 
   //  CSV STUFF ======================================================
 
