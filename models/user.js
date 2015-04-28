c07f15fdd2dccf427f36013f89a844465edcabf5
Christoph Omid Hohnerlein <christoph.o.hohnerlein@campus.tu-berlin.de>
2015-04-28 12:42:25 +0200

    remove email from user model

--- models/user.js
+++ models/user.js
@@ -5,8 +5,6 @@ module.exports = function(sequelize, DataTypes) {
       username: { type: DataTypes.STRING, allowNull: false, unique: true}, // lastname.firstname
       firstname: DataTypes.STRING,
       lastname: DataTypes.STRING,
-      email: {type: DataTypes.STRING,
-              validate:{isEmail: true}},
       role: DataTypes.STRING,
       active: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true},
     }, {
