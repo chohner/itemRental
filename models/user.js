7f1d0de4bc1827650e4b8cd1d11c5606b494d707
Christoph Omid Hohnerlein <christoph.o.hohnerlein@campus.tu-berlin.de>
2015-05-06 00:08:05 +0200

    username as new primaryKey

--- models/user.js
+++ models/user.js
@@ -2,18 +2,14 @@
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-      username: { type: DataTypes.STRING, allowNull: false, unique: true}, // lastname.firstname
+      username: { type: DataTypes.STRING,
+                  primaryKey: true,
+                  allowNull: false,
+                  unique: true}, // lastname.firstname
       firstname: DataTypes.STRING,
       lastname: DataTypes.STRING,
       role: { type: DataTypes.STRING, defaultValue: 'User'},
-      active: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true},
-    }, {
-    classMethods: {
-      associate: function(models) {
-        User.hasMany(models.Item)
-      }
-    }
-  });
-
+      active: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true}
+    });
   return User;
 };
